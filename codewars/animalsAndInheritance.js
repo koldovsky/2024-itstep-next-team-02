9943eddfd9492d72a56e995c39f1899c7822881b
Pasha1007 <61791504+Pasha1007@users.noreply.github.com>
2024-09-18 15:40:40 +0300

    Create animalsAndInheritance.js

---
+++ codewars/animalsAndInheritance.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/56f935002e6c0d55fa000d92
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0,'shark', status);
+  }
+}
+
+class Cat extends Animal {
+ constructor(name, age, status, species) {
+    super(name, age, 4,'cat', status, species);
+  }
+  introduce(){
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, 'dog', status);
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
