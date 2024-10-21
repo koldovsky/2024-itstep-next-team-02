3f940c7238e04d81e89c7032706e92432b563c23
Vitaliy <bering856@gmail.com>
2024-10-21 11:24:55 +0300

    resolved all conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-
-import FlashSale from "./components/flashSales/FlashSale";
-import FlashSaleGoods from "./components/flashSalesGoods/FlashSaleGoods";
-import Slider from "./components/sliderOnHeader/Slider";
-import ViewAllButton from "./components/viewAllButton/ViewAllButton";
+import FlashSale from './components/flashSales/FlashSale';
+import FlashSaleGoods from './components/flashSalesGoods/FlashSaleGoods';
+import Slider from './components/sliderOnHeader/Slider';
+import ViewAllButton from './components/viewAllButton/ViewAllButton';
 
 function Home() {
-  return <div><Slider /><FlashSale/><FlashSaleGoods/><ViewAllButton/></div>;
+  return (
+    <div>
+      <Slider />
+      <FlashSale />
+      <FlashSaleGoods />
+      <ViewAllButton />
+    </div>
+  );
 }
+
+export default Home;
