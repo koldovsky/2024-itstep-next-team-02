7df849ca05b72111063c8313d3b0cfdc5cecdfb5
PavloFurhalo <p.furgalo2006@gmail.com>
2024-10-21 19:17:32 +0300

    changes

---
+++ app/components/categoriesGoods/CategoriesGoods.tsx
@@ -0,0 +1,66 @@
+import styles from './CategoriesGoods.module.css'
+
+
+const CategoriesGoods = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.good}>
+                <div className={styles.frame}>
+                    
+                        <div className={styles.discount}>
+                            icon
+                        </div>
+                    
+                </div>
+            </div>
+            <div className={styles.good}>
+                <div className={styles.frame}>
+                    
+                        <div className={styles.discount}>
+                            icon
+                        </div>
+                    
+                </div>
+            </div>
+            <div className={styles.good}>
+                <div className={styles.frame}>
+                    
+                        <div className={styles.discount}>
+                            icon
+                        </div>
+                    
+                </div>
+            </div>
+            <div className={styles.good}>
+                <div className={styles.frame}>
+                    
+                        <div className={styles.discount}>
+                            icon
+                        </div>
+                    
+                </div>
+            </div>  
+            <div className={styles.good}>
+                <div className={styles.frame}>
+                    
+                        <div className={styles.discount}>
+                            icon
+                        </div>
+                    
+                </div>
+            </div>
+            <div className={styles.good}>
+                <div className={styles.frame}>
+                    
+                        <div className={styles.discount}>
+                            icon
+                        </div>
+                    
+                </div>
+            </div>   
+        </div>
+    );
+  };
+
+  export default CategoriesGoods;
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import Categories from './components/categories/Categories';
+import CategoriesGoods from './components/categoriesGoods/CategoriesGoods';
 import FlashSale from './components/flashSales/FlashSale';
 import FlashSaleGoods from './components/flashSalesGoods/FlashSaleGoods';
+import Line from './components/line/Line';
 import Slider from './components/sliderOnHeader/Slider';
 import ViewAllButton from './components/viewAllButton/ViewAllButton';
 
@@ -10,6 +13,10 @@ function Home() {
       <FlashSale />
       <FlashSaleGoods />
       <ViewAllButton />
+      <Line />
+      <Categories />
+      <CategoriesGoods />
+      <Line />
     </div>
   );
 }
