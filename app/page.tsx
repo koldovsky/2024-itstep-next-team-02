c4a2323ebe4e9c8145c3dc659c99fbb56b6b5fdb
Vitaliy <bering856@gmail.com>
2024-10-21 11:11:29 +0300

    resolve conflict

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+
+import FlashSale from "./components/flashSales/FlashSale";
+import FlashSaleGoods from "./components/flashSalesGoods/FlashSaleGoods";
 import Slider from "./components/sliderOnHeader/Slider";
+import ViewAllButton from "./components/viewAllButton/ViewAllButton";
 
 function Home() {
-  return <div><Slider /></div>;
+  return <div><Slider /><FlashSale/><FlashSaleGoods/><ViewAllButton/></div>;
 }
 
 export default Home;
