146cbae248092cd36802aeaadeb526e0bd3f3d69
AndriyZahvoyskyy <zagvojski5@gmail.com>
2024-10-24 15:32:53 +0300

    AndriyBranch

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import Features from './components/features/features';
 import FlashSale from './components/flashSales/FlashSale';
 import FlashSaleGoods from './components/flashSalesGoods/FlashSaleGoods';
+import NewArrival from './components/newArrival/NewArrival';
+import NewArrivalItems from './components/newArrivalItems/newArrivalItems';
 import Slider from './components/sliderOnHeader/Slider';
 import ViewAllButton from './components/viewAllButton/ViewAllButton';
 
@@ -9,6 +12,9 @@ function Home() {
       <Slider />
       <FlashSale />
       <FlashSaleGoods />
+      <NewArrival/>
+      <NewArrivalItems/>
+      <Features/>
       <ViewAllButton />
     </div>
   );
